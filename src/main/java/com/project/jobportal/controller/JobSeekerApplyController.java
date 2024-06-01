8ae9157899707e859f55cb0743015f65c72f3455
Pranjul Singh <singhpranjul001@gmail.com>
2024-06-01 17:01:47 +0530

    Set the recruiter Dashboard to post and view jobs.

---
+++ src/main/java/com/project/jobportal/controller/JobSeekerApplyController.java
@@ -0,0 +1,30 @@
+package com.project.jobportal.controller;
+
+import com.project.jobportal.entity.JobPostActivity;
+import com.project.jobportal.services.JobPostActivityService;
+import com.project.jobportal.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class JobSeekerApplyController {
+    private final JobPostActivityService jobPostActivityService;
+    private final UsersService usersService;
+
+    @Autowired
+    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UsersService usersService) {
+        this.jobPostActivityService = jobPostActivityService;
+        this.usersService = usersService;
+    }
+
+    @GetMapping("job-details-apply/{id}")
+    public String display(@PathVariable("id") int id, Model model){
+        JobPostActivity jobDetails = jobPostActivityService.getOne(id);
+        model.addAttribute("jobDetails", jobDetails);
+        model.addAttribute("user", usersService.getCurrentUserProfile());
+        return "job-details";
+    }
+}
